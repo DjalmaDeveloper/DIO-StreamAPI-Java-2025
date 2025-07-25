8a8862307708ca821db19d7848785ecdca4065fb
Djalma <57427879+DjalmaDeveloper@users.noreply.github.com>
2025-07-25 16:44:25 -0300

    Desafio 11 Concluído

--- src/stream_api/desafio_10.java
+++ src/stream_api/desafio_10.java
@@ -11,7 +11,9 @@ public class desafio_10 {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-        Map<String, List<Integer>> grupos = numeros.stream().filter(n -> n % 2 != 0 && (n % 3 == 0 || n % 5 == 0)).collect(Collectors.groupingBy(numero -> {
+        Map<String, List<Integer>> grupos = numeros.stream()
+                .filter(n -> n % 2 != 0 && (n % 3 == 0 || n % 5 == 0))
+                .collect(Collectors.groupingBy(numero -> {
             if(numero % 3 == 0 && numero % 5 == 0){
                 return "Múltiplo de 3 e 5";
             }
